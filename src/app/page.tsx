23103f4e979469b01988c0b1a6dbd9971c166bd0
JPML94 <juanpam68@gmail.com>
2025-01-13 22:11:23 -0800

    init ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,28 @@
+import { imageConfigDefault } from "next/dist/shared/lib/image-config";
 import Link from "next/link";
+
+const mockUrls = [
+  "https://nh5ui9lo37.ufs.sh/f/VfYJ3QOr2wKo12eSrwaZaNJvq5jyligI80fYnMCz3eQOwTBb",
+  "https://nh5ui9lo37.ufs.sh/f/VfYJ3QOr2wKojbaSNvlYVGi7RcnmLND5xghyOqXKF32dISls",
+  "https://nh5ui9lo37.ufs.sh/f/VfYJ3QOr2wKob4bvM1xmBIZE0r6dOp3xFcqeD5SP7RvXst1z",
+  "https://nh5ui9lo37.ufs.sh/f/VfYJ3QOr2wKo6RjrFYCq28e4FPsT6bXkrlSNI7xH0caAV3GY"
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-        Hello (gallery in progress)
+    <main className="">
+        <div className="flex flex-wrap gap-4">
+          {[...mockImages, ...mockImages, ...mockImages].map((image) => (  
+            <div key={image.id} className="w-1/2 p-4">
+              <img src={image.url} />
+            </div>
+          ))}
+        </div>
     </main>
   );
 }
